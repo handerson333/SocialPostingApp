d92572547052b3135c1db75222caa210172b6d2f
Hayden Anderson <Hayden.Anderson@rexelusa.com>
2020-05-31 15:17:06 -0700

    first commit, have very basic social posts front end

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MEANcourse'`, () => {
+  it(`should have as title 'SocialPostingApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MEANcourse');
+    expect(app.title).toEqual('SocialPostingApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MEANcourse app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('SocialPostingApp app is running!');
   });
 });
