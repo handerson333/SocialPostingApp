46db2cb8bd1a8cc789d9220c14cb7fca4e036d6a
handerson333 <handerson333@gmail.com>
2020-06-18 23:10:59 -0700

    added form validators for images. added service for image on the back end. added multer

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { FormatWidth } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -41,12 +42,16 @@ export class PostsService {
   }
 
 
-  addPost(title: string, content: string) {
-    const post: Post = { id: null, title, content };
-    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
-      .subscribe((responseDate) => {
-        const id = responseDate.postId;
-        post.id = id;
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title);
+
+    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', postData)
+      .subscribe((responseData) => {
+        const post: Post = { id: responseData.postId, title: title, content: content };
+
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
