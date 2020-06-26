f337b2db4bd8b167127cbb343652f4637c46990b
handerson333 <handerson333@gmail.com>
2020-06-25 21:52:05 -0700

    added tokens and jwt to handle the token creationa nd comparisons also added these to both sign up and login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -25,6 +26,41 @@ router.post("/signup", (req, res, next) => {
         });
       });
   });
+});
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth Failed!'
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth Failed!'
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        'here_is_my_secret',
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Auth Failed!'
+      });
+    });
 });
 
 
