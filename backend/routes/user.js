9e411a13577bef022eabb802f45b847894da766e
handerson333 <handerson333@gmail.com>
2020-06-26 21:24:16 -0700

    add and enforce 1h timeout on login, change token to be saved in local storage

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,68 +1,67 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
+const User = require('../models/user');
 
 const router = express.Router();
 
-
-router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
+router.post('/signup', (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
       email: req.body.email,
-      password: hash
+      password: hash,
     });
-    user.save()
-      .then(result => {
+    user
+      .save()
+      .then((result) => {
         res.status(201).json({
-          message: "user created!",
-          result: result
+          message: 'user created!',
+          result: result,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({
-          error: err
+          error: err,
         });
       });
   });
 });
 
-router.post("/login", (req, res, next) => {
+router.post('/login', (req, res, next) => {
   let fetchedUser;
 
   User.findOne({ email: req.body.email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return res.status(401).json({
-          message: 'Auth Failed!'
+          message: 'Auth Failed!',
         });
       }
       fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
-    .then(result => {
+    .then((result) => {
       if (!result) {
         return res.status(401).json({
-          message: 'Auth Failed!'
+          message: 'Auth Failed!',
         });
       }
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id },
         'here_is_my_secret',
-        { expiresIn: "1h" }
+        { expiresIn: '1h' }
       );
       res.status(200).json({
-        token: token
+        token: token,
+        expiresIn: 3600,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       return res.status(401).json({
-        message: 'Auth Failed!'
+        message: 'Auth Failed!',
       });
     });
 });
-
-
 
 module.exports = router;
