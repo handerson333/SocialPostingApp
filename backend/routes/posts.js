f4771eaa0ad5b09a682ee4f4cee7ebe0e6c93a1e
handerson333 <handerson333@gmail.com>
2020-07-10 14:49:00 -0700

    finish adding dialog and formatting it. Add text to each error. catch edge case errors for each api call

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -39,15 +39,22 @@ router.post(
       imagePath: url + '/images/' + req.file.filename,
       author: req.userData.userId,
     });
-    post.save().then((createdPost) => {
-      res.status(201).json({
-        message: 'Post added successfully',
-        post: {
-          ...createdPost,
-          id: createdPost._id,
-        },
+    post
+      .save()
+      .then((createdPost) => {
+        res.status(201).json({
+          message: 'Post added successfully',
+          post: {
+            ...createdPost,
+            id: createdPost._id,
+          },
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: 'Failed to create a post',
+        });
       });
-    });
   }
 );
 
@@ -68,16 +75,19 @@ router.put(
       imagePath: imagePath,
       author: req.userData.userId,
     });
-    Post.updateOne(
-      { _id: req.params.id, author: req.userData.userId },
-      post
-    ).then((result) => {
-      if (result.nModified > 0) {
-        res.status(200).json({ message: 'Updated successfully' });
-      } else {
-        res.status(401).json({ message: 'Not Authorized!' });
-      }
-    });
+    Post.updateOne({ _id: req.params.id, author: req.userData.userId }, post)
+      .then((result) => {
+        if (result.nModified > 0) {
+          res.status(200).json({ message: 'Updated successfully' });
+        } else {
+          res.status(401).json({ message: 'Not Authorized!' });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: `Couldn't update post`,
+        });
+      });
   }
 );
 
@@ -100,29 +110,44 @@ router.get('', (req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count,
       });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Fetching posts failed',
+      });
     });
 });
 
 router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found!' });
-    }
-  });
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Post not found!' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Fetching post failed',
+      });
+    });
 });
 
 router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, author: req.userData.userId }).then(
-    (result) => {
+  Post.deleteOne({ _id: req.params.id, author: req.userData.userId })
+    .then((result) => {
       if (result.n > 0) {
         res.status(200).json({ message: 'Deletion successful' });
       } else {
         res.status(401).json({ message: 'Not Authorized!' });
       }
-    }
-  );
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Deleting post failed',
+      });
+    });
 });
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -21,9 +21,7 @@ router.post('/signup', (req, res, next) => {
         });
       })
       .catch((err) => {
-        res.status(500).json({
-          error: err,
-        });
+        res.status(500).json({ message: 'Invalid authentication credentials' });
       });
   });
 });
@@ -60,7 +58,7 @@ router.post('/login', (req, res, next) => {
     })
     .catch((err) => {
       return res.status(401).json({
-        message: 'Auth Failed!',
+        message: 'Invalid authentication credentials',
       });
     });
 });
